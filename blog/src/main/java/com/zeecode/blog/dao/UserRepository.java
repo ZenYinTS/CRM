8f5bcd15803351eae1991d4099b0806aae28b4c7
ZenYinTS <383641115@qq.com>
2020-08-31 11:11:46 +0800

    删除多余文件夹

--- blog/src/main/java/com/zeecode/blog/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zeecode.blog.dao;
-
-import com.zeecode.blog.pojo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsernameAndPassword(String username,String password);
-}
--- blog/src/main/java/com/zeecode/blog/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.zeecode.blog.service;
-
-import com.zeecode.blog.pojo.User;
-
-public interface UserService {
-    User checkUser(String username,String password);
-}
